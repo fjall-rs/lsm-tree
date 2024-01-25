7bb11bf9d6047df957103c58eb9d53b6f4f3d289
marvin-j97 <marvin.janke.97@gmail.com>
2024-01-25 15:50:05 +0100

    test: pwd handling

---
+++ tests/tree_reload_pwd.rs
@@ -0,0 +1,44 @@
+use fs_extra::dir::CopyOptions;
+use lsm_tree::{Config, SequenceNumberCounter};
+use test_log::test;
+
+const ITEM_COUNT: usize = 10_000;
+
+#[test]
+fn tree_reload_pwd() -> lsm_tree::Result<()> {
+    let folder_old = tempfile::tempdir()?;
+
+    let seqno = SequenceNumberCounter::default();
+
+    {
+        let tree = Config::new(&folder_old).open()?;
+
+        for x in 0..ITEM_COUNT as u64 {
+            let key = x.to_be_bytes();
+            let value = nanoid::nanoid!();
+            tree.insert(key, value.as_bytes(), seqno.next());
+        }
+
+        tree.flush_active_memtable()?;
+
+        assert_eq!(ITEM_COUNT, tree.len()?);
+    }
+
+    let folder_new = tempfile::tempdir()?;
+    let folder_new_subfolder = folder_new.path().join("deep");
+    std::fs::create_dir_all(&folder_new_subfolder)?;
+
+    fs_extra::dir::move_dir(
+        folder_old,
+        &folder_new_subfolder,
+        &CopyOptions::default().content_only(true).overwrite(true),
+    )
+    .expect("should move");
+
+    {
+        let tree = Config::new(&folder_new_subfolder).open()?;
+        assert_eq!(ITEM_COUNT, tree.len()?);
+    }
+
+    Ok(())
+}
