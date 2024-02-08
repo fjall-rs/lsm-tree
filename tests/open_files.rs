97f80366cb0f986135ef231fe57e9d63a330a10e
marvin-j97 <marvin.janke.97@gmail.com>
2024-02-08 23:07:55 +0100

    add file limit test

---
+++ tests/open_files.rs
@@ -0,0 +1,28 @@
+use lsm_tree::{BlockCache, Config};
+use std::sync::Arc;
+use test_log::test;
+
+#[test]
+fn open_file_limit() {
+    let folder = tempfile::tempdir().unwrap();
+    let tree = Config::new(folder)
+        .block_size(1_024)
+        .block_cache(Arc::new(BlockCache::with_capacity_bytes(0)))
+        .open()
+        .unwrap();
+
+    for _ in 0..512 {
+        for x in 0u16..10 {
+            let key = x.to_be_bytes();
+            tree.insert(key, key, 0);
+        }
+        tree.flush_active_memtable().unwrap();
+    }
+
+    eprintln!("read");
+
+    for _ in 0..5 {
+        assert!(tree.first_key_value().unwrap().is_some());
+        eprintln!("read");
+    }
+}
