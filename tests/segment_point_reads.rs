a2215c6a4cce6e8fed5a1f61c3e601734e5daed0
marvin-j97 <marvin.janke.97@gmail.com>
2024-05-11 15:43:47 +0200

    test: add more mvcc read tests

--- tests/segment_point_reads.rs
+++ tests/segment_point_reads.rs
@@ -24,4 +24,39 @@ fn segment_point_reads() -> lsm_tree::Result<()> {
     Ok(())
 }
 
-// TODO: MVCC (get latest)
+#[test]
+fn segment_point_reads_mvcc() -> lsm_tree::Result<()> {
+    let folder = tempfile::tempdir()?.into_path();
+
+    let tree = Config::new(folder).block_size(1_024).open()?;
+
+    for x in 0..ITEM_COUNT as u64 {
+        let key = x.to_be_bytes();
+        tree.insert(key, "0", 0);
+        tree.insert(key, "1", 1);
+        tree.insert(key, "2", 2);
+    }
+    tree.flush_active_memtable()?;
+
+    for x in 0..ITEM_COUNT as u64 {
+        let key = x.to_be_bytes();
+
+        let item = tree.get_internal_entry(key, true, None)?.unwrap();
+        assert_eq!(item.seqno, 2);
+        assert_eq!(&*item.value, b"2");
+
+        let snapshot = tree.snapshot(3);
+        let item = snapshot.get(key)?.unwrap();
+        assert_eq!(&*item, b"2");
+
+        let snapshot = tree.snapshot(2);
+        let item = snapshot.get(key)?.unwrap();
+        assert_eq!(&*item, b"1");
+
+        let snapshot = tree.snapshot(1);
+        let item = snapshot.get(key)?.unwrap();
+        assert_eq!(&*item, b"0");
+    }
+
+    Ok(())
+}
---
+++ tests/tree_mvcc_point_reads.rs
@@ -0,0 +1,65 @@
+use lsm_tree::Config;
+use test_log::test;
+
+#[test]
+fn tree_point_reads_mvcc() -> lsm_tree::Result<()> {
+    let folder = tempfile::tempdir()?.into_path();
+
+    let tree = Config::new(folder).block_size(1_024).open()?;
+
+    tree.insert("a", "a0", 0);
+    tree.insert("a", "a1", 1);
+    tree.insert("b", "b0", 0);
+    tree.insert("b", "b1", 1);
+
+    tree.flush_active_memtable()?;
+
+    tree.insert("b", "b2", 2);
+    tree.insert("b", "b3", 3);
+    tree.insert("c", "c4", 4);
+
+    tree.flush_active_memtable()?;
+
+    tree.insert("a", "a5", 5);
+
+    assert_eq!(&*tree.get("a")?.unwrap(), b"a5");
+    assert_eq!(&*tree.get("b")?.unwrap(), b"b3");
+    assert_eq!(&*tree.get("c")?.unwrap(), b"c4");
+
+    let snapshot = tree.snapshot(1);
+    assert_eq!(&*snapshot.get("a")?.unwrap(), b"a0");
+    assert_eq!(&*snapshot.get("b")?.unwrap(), b"b0");
+    assert!(snapshot.get("c")?.is_none());
+
+    let snapshot = tree.snapshot(2);
+    assert_eq!(&*snapshot.get("a")?.unwrap(), b"a1");
+    assert_eq!(&*snapshot.get("b")?.unwrap(), b"b1");
+    assert!(snapshot.get("c")?.is_none());
+
+    let snapshot = tree.snapshot(3);
+    assert_eq!(&*snapshot.get("a")?.unwrap(), b"a1");
+    assert_eq!(&*snapshot.get("b")?.unwrap(), b"b2");
+    assert!(snapshot.get("c")?.is_none());
+
+    let snapshot = tree.snapshot(4);
+    assert_eq!(&*snapshot.get("a")?.unwrap(), b"a1");
+    assert_eq!(&*snapshot.get("b")?.unwrap(), b"b3");
+    assert!(snapshot.get("c")?.is_none());
+
+    let snapshot = tree.snapshot(5);
+    assert_eq!(&*snapshot.get("a")?.unwrap(), b"a1");
+    assert_eq!(&*snapshot.get("b")?.unwrap(), b"b3");
+    assert_eq!(&*snapshot.get("c")?.unwrap(), b"c4");
+
+    let snapshot = tree.snapshot(6);
+    assert_eq!(&*snapshot.get("a")?.unwrap(), b"a5");
+    assert_eq!(&*snapshot.get("b")?.unwrap(), b"b3");
+    assert_eq!(&*snapshot.get("c")?.unwrap(), b"c4");
+
+    let snapshot = tree.snapshot(100);
+    assert_eq!(&*snapshot.get("a")?.unwrap(), b"a5");
+    assert_eq!(&*snapshot.get("b")?.unwrap(), b"b3");
+    assert_eq!(&*snapshot.get("c")?.unwrap(), b"c4");
+
+    Ok(())
+}
