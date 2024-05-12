9ce96ac2be4bbf36edb7f31d24a683392dddaa86
marvin-j97 <marvin.janke.97@gmail.com>
2024-05-12 20:34:54 +0200

    remove test debug log

--- tests/tree_disjoint_point_read.rs
+++ tests/tree_disjoint_point_read.rs
@@ -71,8 +71,6 @@ fn tree_disjoint_point_read_multiple_levels() -> lsm_tree::Result<()> {
     tree.insert("j", "j", 0);
     tree.flush_active_memtable()?;
 
-    log::error!("{:#?}", tree.levels.read().expect("asdasd").levels);
-
     for key in [b"z", b"b", b"c", b"d", b"e", b"f"] {
         let value = tree.get(key).unwrap().unwrap();
         assert_eq!(&*value, key)
