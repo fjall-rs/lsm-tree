0eaf7fda92da8f0fd214b822195c4ebe9bba8c4b
marvin-j97 <marvin.janke.97@gmail.com>
2024-05-12 21:17:38 +0200

    adjust examples script

--- compile_examples.mjs
+++ compile_examples.mjs
@@ -8,28 +8,6 @@ for (const exampleName of await readdir(examplesFolder)) {
   const folder = resolve(examplesFolder, exampleName);
 
   {
-    const proc = spawn("cargo build", {
-      cwd: folder,
-      shell: true,
-    });
-
-    proc.stdout.on("data", buf => console.log(String(buf)));
-    proc.stderr.on("data", buf => console.error(String(buf)));
-
-    await new Promise((resolve, _) => {
-      proc.on("exit", () => {
-        if (proc.exitCode > 0) {
-          console.error(`${folder} FAILED`);
-          process.exit(1);
-        }
-        else {
-          resolve();
-        }
-      })
-    });
-  }
-
-  {
     const proc = spawn("cargo test", {
       cwd: folder,
       shell: true,
