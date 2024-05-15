1a5071120aeeccf3d59036d2bea6ea5d316393d4
marvin-j97 <marvin.janke.97@gmail.com>
2024-05-15 12:37:34 +0200

    test: v1 reload fixture

---
+++ tests/tree_load_fixture.rs
@@ -0,0 +1,14 @@
+use lsm_tree::Config;
+use test_log::test;
+
+#[test]
+fn tree_load_v1() -> lsm_tree::Result<()> {
+    let folder = "test_fixture/v1_tree";
+
+    let tree = Config::new(folder).open()?;
+    assert_eq!(8, tree.len()?);
+
+    // TODO: call Tree::verify
+
+    Ok(())
+}
