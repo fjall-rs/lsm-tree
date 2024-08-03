3177f13bbf59a691b3c57fae354301ac7880b241
marvin-j97 <marvin.janke.97@gmail.com>
2024-08-04 01:35:12 +0200

    fix memtable bench

--- benches/memtable.rs
+++ benches/memtable.rs
@@ -3,20 +3,19 @@ use lsm_tree::{InternalValue, MemTable};
 use nanoid::nanoid;
 
 fn memtable_get_upper_bound(c: &mut Criterion) {
+    let memtable = MemTable::default();
+
+    for _ in 0..1_000_000 {
+        memtable.insert(InternalValue::from_components(
+            format!("abc_{}", nanoid!()).as_bytes(),
+            vec![],
+            0,
+            lsm_tree::ValueType::Value,
+        ));
+    }
+
     c.bench_function("memtable get", |b| {
-        let memtable = MemTable::default();
-
-        for _ in 0..1_000_000 {
-            memtable.insert(InternalValue::from_components(
-                format!("abc_{}", nanoid!()).as_bytes(),
-                vec![],
-                0,
-                lsm_tree::ValueType::Value,
-            ));
-        }
-
         b.iter(|| {
-            eprintln!("asd");
             memtable.get("abc", None);
         });
     });
