7c791c1c23c84a66ab56f71d00b99738640c56e2
marvin-j97 <marvin.janke.97@gmail.com>
2024-08-06 23:06:38 +0200

    update deps

--- src/lib.rs
+++ src/lib.rs
@@ -93,6 +93,7 @@
 #![warn(clippy::pedantic, clippy::nursery)]
 #![warn(clippy::expect_used)]
 #![allow(clippy::missing_const_for_fn)]
+#![warn(clippy::multiple_crate_versions)]
 
 macro_rules! fail_iter {
     ($e:expr) => {
