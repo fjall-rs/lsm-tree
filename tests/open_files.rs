01b392030a57783c3c1920c36bbda4e56d78a742
marvin-j97 <marvin.janke.97@gmail.com>
2024-08-08 16:20:38 +0200

    fix test

--- tests/open_files.rs
+++ tests/open_files.rs
@@ -3,22 +3,24 @@ use std::sync::Arc;
 use test_log::test;
 
 #[test]
-fn open_file_limit() {
-    let folder = tempfile::tempdir_in(".test").unwrap();
+fn open_file_limit() -> lsm_tree::Result<()> {
+    std::fs::create_dir_all(".test")?;
+    let folder = tempfile::tempdir_in(".test")?;
 
     let tree = Config::new(folder)
         .block_size(1_024)
         .block_cache(Arc::new(BlockCache::with_capacity_bytes(0)))
-        .open()
-        .unwrap();
+        .open()?;
 
     for _ in 0..2_048 {
         let key = 0u64.to_be_bytes();
         tree.insert(key, key, 0);
-        tree.flush_active_memtable().unwrap();
+        tree.flush_active_memtable()?;
     }
 
     for _ in 0..5 {
-        assert!(tree.first_key_value().unwrap().is_some());
+        assert!(tree.first_key_value()?.is_some());
     }
+
+    Ok(())
 }
