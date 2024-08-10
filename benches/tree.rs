4e97151552714e280d16e47fd50853ea8fe613b8
marvin-j97 <marvin.janke.97@gmail.com>
2024-08-11 00:29:30 +0200

    refactor: benches

--- benches/tree.rs
+++ benches/tree.rs
@@ -1,5 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use lsm_tree::{AbstractTree, BlockCache, Config, Slice};
+use lsm_tree::{AbstractTree, BlockCache, Config};
 use std::sync::Arc;
 use tempfile::tempdir;
 
@@ -169,62 +169,6 @@ fn scan_vs_prefix(c: &mut Criterion) {
                 assert_eq!(iter.rev().count(), 1000);
             });
         });
-    }
-}
-
-fn tli_find_item(c: &mut Criterion) {
-    use lsm_tree::segment::block_index::{
-        block_handle::KeyedBlockHandle, top_level::TopLevelIndex,
-    };
-
-    let mut group = c.benchmark_group("TLI find item");
-
-    for item_count in [10u64, 100, 1_000, 1_000_000] {
-        let items = {
-            let mut items = Vec::with_capacity(item_count as usize);
-
-            for x in 0..item_count {
-                items.push(KeyedBlockHandle {
-                    end_key: x.to_be_bytes().into(),
-                    offset: x,
-                });
-            }
-
-            items
-        };
-
-        let index = TopLevelIndex::from_boxed_slice(items.into());
-
-        group.bench_function(
-            format!("TLI get_next_block_handle ({item_count} items)"),
-            |b| {
-                let key = (item_count / 10 * 6).to_be_bytes();
-                let expected: Slice = (item_count / 10 * 6 + 1).to_be_bytes().into();
-
-                let block = index.get_lowest_block_containing_key(&key).unwrap();
-
-                b.iter(|| {
-                    assert_eq!(
-                        expected,
-                        index.get_next_block_handle(block.offset).unwrap().end_key
-                    );
-                })
-            },
-        );
-
-        group.bench_function(
-            format!("TLI get_block_containing_item ({item_count} items)"),
-            |b| {
-                let key = (item_count / 10 * 6).to_be_bytes();
-
-                b.iter(|| {
-                    assert_eq!(
-                        key,
-                        &*index.get_lowest_block_containing_key(&key).unwrap().end_key
-                    );
-                })
-            },
-        );
     }
 }
 
@@ -416,7 +360,6 @@ criterion_group!(
     full_scan,
     scan_vs_query,
     scan_vs_prefix,
-    tli_find_item,
     tree_get_pairs,
 );
 criterion_main!(benches);
