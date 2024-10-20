dd30c1ba6aa67aecc74479da796e4586ed998fc1
Marvin <33938500+marvin-j97@users.noreply.github.com>
2024-10-20 16:50:31 +0200

    Update mod.rs

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -152,6 +152,8 @@ impl AbstractTree for Tree {
         {
             if self.config.bloom_bits_per_key >= 0 {
                 segment_writer = segment_writer.use_bloom_policy(
+                    // TODO: increase to 0.00001 when https://github.com/fjall-rs/lsm-tree/issues/63
+                    // is fixed
                     crate::segment::writer::BloomConstructionPolicy::FpRate(0.0001),
                 );
             }
