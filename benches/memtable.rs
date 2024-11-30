b41714774b3cde1ad9159c12461690810304eb7f
marvin-j97 <marvin.janke.97@gmail.com>
2024-11-30 02:50:51 +0100

    change memtable bench

--- benches/memtable.rs
+++ benches/memtable.rs
@@ -2,23 +2,51 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use lsm_tree::{InternalValue, Memtable};
 use nanoid::nanoid;
 
-fn memtable_get_upper_bound(c: &mut Criterion) {
-    let memtable = Memtable::default();
+fn memtable_get(c: &mut Criterion) {
+    c.bench_function("memtable get", |b| {
+        let memtable = Memtable::default();
 
-    for _ in 0..1_000_000 {
         memtable.insert(InternalValue::from_components(
-            format!("abc_{}", nanoid!()).as_bytes(),
+            "abc_w5wa35aw35naw",
             vec![],
             0,
             lsm_tree::ValueType::Value,
         ));
-    }
+
+        for _ in 0..1_000_000 {
+            memtable.insert(InternalValue::from_components(
+                format!("abc_{}", nanoid!()).as_bytes(),
+                vec![],
+                0,
+                lsm_tree::ValueType::Value,
+            ));
+        }
 
     c.bench_function("memtable get", |b| {
         b.iter(|| {
-            memtable.get("abc", None);
+            memtable.get("abc_w5wa35aw35naw", None);
         });
     });
 }
-criterion_group!(benches, memtable_get_upper_bound);
+
+fn memtable_highest_seqno(c: &mut Criterion) {
+    c.bench_function("memtable highest seqno", |b| {
+        let memtable = Memtable::default();
+
+        for x in 0..100_000 {
+            memtable.insert(InternalValue::from_components(
+                format!("abc_{}", nanoid!()).as_bytes(),
+                vec![],
+                x,
+                lsm_tree::ValueType::Value,
+            ));
+        }
+
+        b.iter(|| {
+            assert_eq!(Some(99_999), memtable.get_highest_seqno());
+        });
+    });
+}
+
+criterion_group!(benches, memtable_get, memtable_highest_seqno);
 criterion_main!(benches);
