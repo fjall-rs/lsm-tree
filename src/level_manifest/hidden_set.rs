186fd9feb176da3447d45b206cb9179cc7a3b6e7
Marvin <33938500+marvin-j97@users.noreply.github.com>
2024-12-04 23:22:14 +0100

    Update hidden_set.rs

--- src/level_manifest/hidden_set.rs
+++ src/level_manifest/hidden_set.rs
@@ -1,7 +1,12 @@
 use crate::segment::meta::SegmentId;
-
 use crate::HashSet;
 
+/// The hidden set keeps track of which segments are currently being compacted
+///
+/// When a segment is hidden (being compacted), no other compaction task can include that
+/// segment, or it will be declined to be run.
+///
+/// If a compaction task fails, the segments are shown again (removed from the hidden set).
 #[derive(Clone)]
 pub(crate) struct HiddenSet {
     pub(crate) set: HashSet<SegmentId>,
