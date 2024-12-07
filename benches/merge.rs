4c2754cdf151773a8e663ff4b87421ec31ec3fc6
marvin-j97 <marvin.janke.97@gmail.com>
2024-12-07 20:05:30 +0100

    adjust bench

--- benches/merge.rs
+++ benches/merge.rs
@@ -4,36 +4,38 @@ use lsm_tree::{InternalValue, Memtable};
 use nanoid::nanoid;
 
 fn merger(c: &mut Criterion) {
-    let memtables = (0..30)
-        .map(|_| {
-            let table = Memtable::default();
+    for num in [2, 4, 8, 16, 30] {
+        c.bench_function(&format!("Merge {num}"), |b| {
+            let memtables = (0..num)
+                .map(|_| {
+                    let table = Memtable::default();
 
-            for _ in 0..100 {
-                table.insert(InternalValue::from_components(
-                    nanoid!(),
-                    vec![],
-                    0,
-                    lsm_tree::ValueType::Value,
-                ));
-            }
+                    for _ in 0..100 {
+                        table.insert(InternalValue::from_components(
+                            nanoid!(),
+                            vec![],
+                            0,
+                            lsm_tree::ValueType::Value,
+                        ));
+                    }
 
-            table
-        })
-        .collect::<Vec<_>>();
+                    table
+                })
+                .collect::<Vec<_>>();
 
-    c.bench_function("Merger", |b| {
-        b.iter_with_large_drop(|| {
-            let iters = memtables
-                .iter()
-                .map(|x| x.iter().map(Ok))
-                .map(|x| Box::new(x) as BoxedIterator<'_>)
-                .collect();
+            b.iter_with_large_drop(|| {
+                let iters = memtables
+                    .iter()
+                    .map(|x| x.iter().map(Ok))
+                    .map(|x| Box::new(x) as BoxedIterator<'_>)
+                    .collect();
 
-            let merger = Merger::new(iters);
+                let merger = Merger::new(iters);
 
-            assert_eq!(30 * 100, merger.count());
-        })
-    });
+                assert_eq!(num * 100, merger.count());
+            })
+        });
+    }
 }
 
 criterion_group!(benches, merger);
