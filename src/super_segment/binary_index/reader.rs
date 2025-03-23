859999f500942223dd8c3fe7da15653ad3aad3aa
marvin-j97 <marvin.janke.97@gmail.com>
2025-03-23 02:37:19 +0100

    refactor

--- src/super_segment/binary_index/reader.rs
+++ src/super_segment/binary_index/reader.rs
@@ -2,6 +2,8 @@ use byteorder::{BigEndian, ReadBytesExt};
 
 type FencePtr = u32;
 
+const FENCE_PTR_SIZE: usize = std::mem::size_of::<FencePtr>();
+
 pub struct Reader<'a> {
     bytes: &'a [u8],
 }
@@ -9,16 +11,16 @@ pub struct Reader<'a> {
 impl<'a> Reader<'a> {
     pub fn new(bytes: &'a [u8], offset: usize, len: usize) -> Self {
         Self {
-            bytes: &bytes[offset..(offset + len * std::mem::size_of::<FencePtr>())],
+            bytes: &bytes[offset..(offset + len * FENCE_PTR_SIZE)],
         }
     }
 
     pub fn len(&self) -> usize {
-        self.bytes.len() / std::mem::size_of::<FencePtr>()
+        self.bytes.len() / FENCE_PTR_SIZE
     }
 
     pub fn get(&self, idx: usize) -> FencePtr {
-        let offset = idx * std::mem::size_of::<FencePtr>();
+        let offset = idx * FENCE_PTR_SIZE;
 
         let mut bytes = self.bytes.get(offset..).expect("should be in array");
         bytes.read_u32::<BigEndian>().expect("should read")
