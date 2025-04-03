b273367fd093782997695e735be3804253d2cb3a
marvin-j97 <marvin.janke.97@gmail.com>
2025-04-03 18:55:43 +0200

    refactor: bloom

--- src/bloom/bit_array.rs
+++ src/bloom/bit_array.rs
@@ -4,23 +4,17 @@
 
 const BIT_MASK: u8 = 0b1000_0000_u8;
 
-/// Gets a bit from the byte
+/// Gets a bit from the byte.
 fn get_bit(byte: u8, idx: usize) -> bool {
     let bit_mask = BIT_MASK >> idx;
-
     let masked = byte & bit_mask;
     masked > 0
 }
 
-/// Sets a bit in the byte
-fn set_bit(byte: u8, idx: usize, value: bool) -> u8 {
+/// Enables the given bit in the byte.
+fn enable_bit(byte: u8, idx: usize) -> u8 {
     let bit_mask = BIT_MASK >> idx;
-
-    if value {
-        byte | bit_mask
-    } else {
-        byte & !bit_mask
-    }
+    byte | bit_mask
 }
 
 /// Fixed-size bit array
@@ -28,32 +22,35 @@ fn set_bit(byte: u8, idx: usize, value: bool) -> u8 {
 pub struct BitArray(Box<[u8]>);
 
 impl BitArray {
+    /// Creates a new bit array with the given size in bytes.
     #[must_use]
     pub fn with_capacity(bytes: usize) -> Self {
         let vec = vec![0; bytes];
         Self(vec.into_boxed_slice())
     }
 
+    /// Treats the given byte array as bit array.
     #[must_use]
     pub fn from_bytes(bytes: Box<[u8]>) -> Self {
         Self(bytes)
     }
 
+    /// Returns the inner data.
     #[must_use]
     pub fn bytes(&self) -> &[u8] {
         &self.0
     }
 
-    /// Sets the i-th bit
-    pub fn set(&mut self, idx: usize, val: bool) {
+    /// Sets the i-th bit to `true`.
+    pub fn enable(&mut self, idx: usize) {
         let byte_idx = idx / 8;
         let byte = self.0.get_mut(byte_idx).expect("should be in bounds");
 
-        let bit_idx = idx % 8;
-        *byte = set_bit(*byte, bit_idx, val);
+        let bit_idx: usize = idx % 8;
+        *byte = enable_bit(*byte, bit_idx);
     }
 
-    /// Gets the i-th bit
+    /// Gets the i-th bit.
     #[must_use]
     pub fn get(&self, idx: usize) -> bool {
         let byte_idx = idx / 8;
@@ -71,27 +68,14 @@ mod tests {
 
     #[test]
     fn bit_set_true() {
-        assert_eq!(0b0000_0010, set_bit(0, 6, true));
-        assert_eq!(0b1000_0000, set_bit(0, 0, true));
-        assert_eq!(0b0100_0000, set_bit(0, 1, true));
-        assert_eq!(0b0100_0110, set_bit(0b0000_0110, 1, true));
-    }
-
-    #[test]
-    fn bit_set_false() {
-        assert_eq!(0b1111_1101, set_bit(0xFF, 6, false));
-        assert_eq!(0b0111_1111, set_bit(0xFF, 0, false));
-        assert_eq!(0b1011_1111, set_bit(0xFF, 1, false));
-
-        assert_eq!(0b0000_0110, set_bit(0b0100_0110, 1, false));
+        assert_eq!(0b0000_0010, enable_bit(0, 6));
+        assert_eq!(0b1000_0000, enable_bit(0, 0));
+        assert_eq!(0b0100_0000, enable_bit(0, 1));
+        assert_eq!(0b0100_0110, enable_bit(0b0000_0110, 1));
     }
 
     #[test]
     fn bit_set_get() {
-        assert_eq!(0b1111_1101, set_bit(0xFF, 6, false));
-        assert_eq!(0b0111_1111, set_bit(0xFF, 0, false));
-        assert_eq!(0b1011_1111, set_bit(0xFF, 1, false));
-
         assert!(!get_bit(0b0100_0110, 0));
         assert!(get_bit(0b0100_0110, 1));
         assert!(get_bit(0b0100_0110, 6));
