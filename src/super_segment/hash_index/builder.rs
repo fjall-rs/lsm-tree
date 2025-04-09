6cfbf4061681cc6b0f633b5133696fc1d08cf822
marvin-j97 <marvin.janke.97@gmail.com>
2025-04-09 18:54:38 +0200

    finish block hash index

--- src/super_segment/hash_index/builder.rs
+++ src/super_segment/hash_index/builder.rs
@@ -3,35 +3,36 @@ use byteorder::WriteBytesExt;
 
 pub const MAX_POINTERS_FOR_HASH_INDEX: u8 = u8::MAX - 2;
 
+/// Builds a block hash index
 #[derive(Debug)]
 pub struct Builder(Vec<u8>);
 
 impl Builder {
+    /// Initializes a new builder with the given amount of buckets.
     pub fn new(bucket_count: u32) -> Self {
         Self(vec![MARKER_FREE; bucket_count as usize])
     }
 
     // NOTE: We know the hash index has a bucket count <= u8
     #[allow(clippy::cast_possible_truncation)]
-    /// Returns the number of buckets
+    /// Returns the number of buckets.
     pub fn bucket_count(&self) -> u32 {
         self.0.len() as u32
     }
 
+    /// Tries to map the given key to the binary index position.
     pub fn set(&mut self, key: &[u8], binary_index_pos: u8) -> bool {
         let bucket_pos = calculate_bucket_position(key, self.bucket_count());
 
-        // SAFETY: We used modulo
-        #[warn(unsafe_code)]
+        // SAFETY: We use modulo in `calculate_bucket_position`
+        #[allow(unsafe_code)]
         let curr_marker = unsafe { *self.0.get_unchecked(bucket_pos) };
 
         match curr_marker {
             MARKER_CONFLICT => false,
             MARKER_FREE => {
-                // NOTE: Free slot
-
                 // SAFETY: We previously asserted that the slot exists
-                #[warn(unsafe_code)]
+                #[allow(unsafe_code)]
                 unsafe {
                     *self.0.get_unchecked_mut(bucket_pos) = binary_index_pos;
                 }
@@ -47,7 +48,7 @@ impl Builder {
                 // NOTE: Mark as conflicted
 
                 // SAFETY: We previously asserted that the slot exists
-                #[warn(unsafe_code)]
+                #[allow(unsafe_code)]
                 unsafe {
                     *self.0.get_unchecked_mut(bucket_pos) = MARKER_CONFLICT;
                 }
@@ -57,78 +58,19 @@ impl Builder {
         }
     }
 
+    /// Consumes the builder, returning its raw bytes.
+    ///
+    /// Only used for tests
     #[cfg(test)]
     pub fn into_inner(self) -> Vec<u8> {
         self.0
     }
 
+    /// Appends the raw index bytes to a writer.
     pub fn write<W: std::io::Write>(self, writer: &mut W) -> std::io::Result<()> {
         for byte in self.0 {
             writer.write_u8(byte)?;
         }
         Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use test_log::test;
-
-    #[test]
-    fn v3_hash_index_simple() {
-        let mut hash_index = Builder::new(100);
-
-        hash_index.set(b"a", 5);
-        hash_index.set(b"b", 8);
-        hash_index.set(b"c", 10);
-
-        // NOTE: Hash index bytes need to be consistent across machines and compilations etc.
-        assert_eq!(
-            [
-                254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 10, 254, 254, 254, 8, 254,
-                254, 254, 5, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254,
-                254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254,
-                254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254,
-                254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254,
-                254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254,
-                254, 254
-            ],
-            &*hash_index.into_inner()
-        );
-    }
-
-    #[test]
-    fn v3_hash_index_conflict() {
-        let mut hash_index = Builder::new(1);
-
-        hash_index.set(b"a", 5);
-        hash_index.set(b"b", 8);
-
-        // NOTE: Hash index bytes need to be consistent across machines and compilations etc.
-        assert_eq!([255], &*hash_index.into_inner());
-    }
-
-    #[test]
-    fn v3_hash_index_same_offset() {
-        let mut hash_index = Builder::new(1);
-
-        hash_index.set(b"a", 5);
-        hash_index.set(b"b", 5);
-
-        // NOTE: Hash index bytes need to be consistent across machines and compilations etc.
-        assert_eq!([5], &*hash_index.into_inner());
-    }
-
-    #[test]
-    fn v3_hash_index_mix() {
-        let mut hash_index = Builder::new(1);
-
-        hash_index.set(b"a", 5);
-        hash_index.set(b"b", 5);
-        hash_index.set(b"c", 6);
-
-        // NOTE: Hash index bytes need to be consistent across machines and compilations etc.
-        assert_eq!([255], &*hash_index.into_inner());
     }
 }
--- src/super_segment/hash_index/reader.rs
+++ src/super_segment/hash_index/reader.rs
@@ -1,26 +1,67 @@
 use super::{calculate_bucket_position, MARKER_CONFLICT, MARKER_FREE};
 
+/// Hash index lookup result
+#[derive(Debug, Eq, PartialEq)]
+pub enum Lookup {
+    /// Key is found, can skip the binary index search - fast path
+    Found(u8),
+
+    /// Key's bucket was still FREE, so it definitely does not exist
+    NotFound,
+
+    /// Key is conflicted - we need to look in the binary index instead - slow path
+    Conflicted,
+}
+
+/// Helper to read from an embedded block hash index
 pub struct Reader<'a>(&'a [u8]);
 
 impl<'a> Reader<'a> {
-    pub fn new(bytes: &'a [u8], offset: usize, len: usize) -> Self {
-        Self(&bytes[offset..(offset + len)])
+    /// Initializes a new hash index reader.
+    pub fn new(bytes: &'a [u8], offset: u32, len: u32) -> Self {
+        let offset = offset as usize;
+        let len = len as usize;
+        let end = offset + len;
+
+        // NOTE: We consider the caller to be trustworthy
+        #[warn(clippy::indexing_slicing)]
+        Self(&bytes[offset..end])
     }
 
-    pub fn get(&self, key: &[u8]) -> Option<u8> {
-        // NOTE: We know the hash index has a bucket count <= u8
+    // NOTE: Not used for performance reasons, so no need to be hyper-optimized
+    #[allow(clippy::naive_bytecount)]
+    /// Returns the amount of empty slots in the hash index.
+    pub fn free_count(&self) -> usize {
+        self.0.iter().filter(|&&byte| byte == MARKER_FREE).count()
+    }
+
+    // NOTE: Not used for performance reasons, so no need to be hyper-optimized
+    #[allow(clippy::naive_bytecount)]
+    /// Returns the amount of conflict markers in the hash index.
+    pub fn conflict_count(&self) -> usize {
+        self.0
+            .iter()
+            .filter(|&&byte| byte == MARKER_CONFLICT)
+            .count()
+    }
+
+    /// Returns the binary index position if the key is not conflicted.
+    pub fn get(&self, key: &[u8]) -> Lookup {
+        // NOTE: Even with very high hash ratio, there will be nearly enough items to
+        // cause us to create u32 buckets
         #[allow(clippy::cast_possible_truncation)]
         let bucket_count = self.0.len() as u32;
 
         let bucket_pos = calculate_bucket_position(key, bucket_count);
 
-        // SAFETY: We used modulo
-        #[warn(unsafe_code)]
+        // SAFETY: We use modulo in `calculate_bucket_position`
+        #[allow(unsafe_code)]
         let marker = unsafe { *self.0.get_unchecked(bucket_pos) };
 
         match marker {
-            MARKER_CONFLICT | MARKER_FREE => None,
-            idx => Some(idx),
+            MARKER_CONFLICT => Lookup::Conflicted,
+            MARKER_FREE => Lookup::NotFound,
+            idx => Lookup::Found(idx),
         }
     }
 }
