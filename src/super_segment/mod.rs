97e57aa1551541f076019c5b8001f6a8bc6948d5
marvin-j97 <marvin.janke.97@gmail.com>
2025-04-10 01:24:32 +0200

    wip

--- src/super_segment/mod.rs
+++ src/super_segment/mod.rs
@@ -3,12 +3,12 @@
 // (found in the LICENSE-* files in the repository)
 
 pub(crate) mod binary_index;
-mod block;
+pub mod block;
 pub(crate) mod data_block;
 pub(crate) mod hash_index;
 // mod index_block;
 pub(crate) mod util;
 
-pub use block::{Block, Header};
+pub use block::Block;
 pub use data_block::DataBlock;
 // pub use index_block::IndexBlock;
