f9f6364de0c40907db33be8a0f452edf2bc97965
marvin-j97 <marvin.janke.97@gmail.com>
2025-07-27 03:37:20 +0200

    license

--- src/double_ended_peekable.rs
+++ src/double_ended_peekable.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2024-present, fjall-rs
+// This source code is licensed under both the Apache 2.0 and MIT License
+// (found in the LICENSE-* files in the repository)
+
 //! A fork of <https://github.com/dodomorandi/double-ended-peekable>
 //! to allow accessing the inner type
 //!
