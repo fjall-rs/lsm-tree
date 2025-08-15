aa00a7521c2a03c306c6665a256c1e74f3339717
Marvin <33938500+marvin-j97@users.noreply.github.com>
2025-08-15 19:00:44 +0200

    prevent combinatoric explosion in leveled compaction picking
    oops

--- src/compaction/leveled.rs
+++ src/compaction/leveled.rs
@@ -23,6 +23,7 @@ fn pick_minimal_compaction(
     curr_level: &Level,
     next_level: &Level,
     hidden_set: &HiddenSet,
+    segment_base_size: u64,
 ) -> Option<(HashSet<SegmentId>, bool)> {
     // assert!(curr_level.is_disjoint, "Lx is not disjoint");
     // assert!(next_level.is_disjoint, "Lx+1 is not disjoint");
@@ -52,6 +53,18 @@ fn pick_minimal_compaction(
     };
 
     for window in next_level.growing_windows() {
+        let next_level_size = next_level
+            .iter()
+            .map(|x| x.metadata.file_size)
+            .sum::<u64>();
+        
+        // Cap at 50x segments per compaction for now
+        if next_level_size > (50 * segment_base_size) {
+            // NOTE: Break out of the loop, at this point, all compactions are too large anyway
+            // so we can escape early
+            break;
+        }
+        
         if hidden_set.is_blocked(window.iter().map(Segment::id)) {
             // IMPORTANT: Compaction is blocked because of other
             // on-going compaction
@@ -257,7 +270,7 @@ impl CompactionStrategy for Strategy {
                 };
 
                 let Some((segment_ids, can_trivial_move)) =
-                    pick_minimal_compaction(level, next_level, levels.hidden_set())
+                    pick_minimal_compaction(level, next_level, levels.hidden_set(), self.target_size)
                 else {
                     break;
                 };
