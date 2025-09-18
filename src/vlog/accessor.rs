f50929cba4b9b9741e8c9a75c54037712d0fb2e2
marvin-j97 <marvin.janke.97@gmail.com>
2025-09-18 18:26:11 +0200

    license

--- src/vlog/accessor.rs
+++ src/vlog/accessor.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2024-present, fjall-rs
+// This source code is licensed under both the Apache 2.0 and MIT License
+// (found in the LICENSE-* files in the repository)
+
 use crate::{
     vlog::{blob_file::writer::BLOB_HEADER_LEN, BlobFileId, ValueHandle},
     BlobFile, Cache, DescriptorTable, GlobalSegmentId, Slice, UserValue,
