e34684ee21cba000a0e8e73d94cca650fc4784e0
marvin-j97 <marvin.janke.97@gmail.com>
2025-09-18 18:06:14 +0200

    wip

--- src/blob_tree/mod.rs
+++ src/blob_tree/mod.rs
@@ -2,7 +2,6 @@
 // This source code is licensed under both the Apache 2.0 and MIT License
 // (found in the LICENSE-* files in the repository)
 
-mod compression;
 mod gc;
 pub mod index;
 pub mod value;
