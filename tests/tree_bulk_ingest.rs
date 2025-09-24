51de6cd8cbd6b99b14ddde332ea02ac81e9202af
marvin-j97 <marvin.janke.97@gmail.com>
2025-09-24 19:23:37 +0200

    wip

--- tests/tree_bulk_ingest.rs
+++ tests/tree_bulk_ingest.rs
@@ -6,202 +6,208 @@ const ITEM_COUNT: usize = 100_000;
 #[test]
 #[ignore]
 fn tree_bulk_ingest() -> lsm_tree::Result<()> {
-    let folder = tempfile::tempdir()?;
+    todo!()
 
-    let tree = Config::new(folder).open()?;
+    // let folder = tempfile::tempdir()?;
 
-    let seqno = SequenceNumberCounter::default();
-    let visible_seqno = SequenceNumberCounter::default();
+    // let tree = Config::new(folder).open()?;
 
-    tree.ingest(
-        (0..ITEM_COUNT as u64).map(|x| {
-            let k = x.to_be_bytes();
-            let v = nanoid::nanoid!();
-            (k.into(), v.into())
-        }),
-        &seqno,
-        &visible_seqno,
-    )?;
+    // let seqno = SequenceNumberCounter::default();
+    // let visible_seqno = SequenceNumberCounter::default();
 
-    assert_eq!(tree.len(SeqNo::MAX, None)?, ITEM_COUNT);
-    assert_eq!(
-        tree.iter(SeqNo::MAX, None).flat_map(|x| x.key()).count(),
-        ITEM_COUNT,
-    );
-    assert_eq!(
-        tree.iter(SeqNo::MAX, None)
-            .rev()
-            .flat_map(|x| x.key())
-            .count(),
-        ITEM_COUNT,
-    );
+    // tree.ingest(
+    //     (0..ITEM_COUNT as u64).map(|x| {
+    //         let k = x.to_be_bytes();
+    //         let v = nanoid::nanoid!();
+    //         (k.into(), v.into())
+    //     }),
+    //     &seqno,
+    //     &visible_seqno,
+    // )?;
 
-    Ok(())
+    // assert_eq!(tree.len(SeqNo::MAX, None)?, ITEM_COUNT);
+    // assert_eq!(
+    //     tree.iter(SeqNo::MAX, None).flat_map(|x| x.key()).count(),
+    //     ITEM_COUNT,
+    // );
+    // assert_eq!(
+    //     tree.iter(SeqNo::MAX, None)
+    //         .rev()
+    //         .flat_map(|x| x.key())
+    //         .count(),
+    //     ITEM_COUNT,
+    // );
+
+    // Ok(())
 }
 
 #[test]
 #[ignore]
 fn tree_copy() -> lsm_tree::Result<()> {
-    let folder = tempfile::tempdir()?;
-    let src = Config::new(folder).open()?;
+    todo!()
 
-    let seqno = SequenceNumberCounter::default();
-    let visible_seqno = SequenceNumberCounter::default();
+    //     let folder = tempfile::tempdir()?;
+    //     let src = Config::new(folder).open()?;
 
-    src.ingest(
-        (0..ITEM_COUNT as u64).map(|x| {
-            let k = x.to_be_bytes();
-            let v = nanoid::nanoid!();
-            (k.into(), v.into())
-        }),
-        &seqno,
-        &visible_seqno,
-    )?;
+    //     let seqno = SequenceNumberCounter::default();
+    //     let visible_seqno = SequenceNumberCounter::default();
 
-    assert_eq!(src.len(SeqNo::MAX, None)?, ITEM_COUNT);
-    assert_eq!(
-        src.iter(SeqNo::MAX, None).flat_map(|x| x.key()).count(),
-        ITEM_COUNT,
-    );
-    assert_eq!(
-        src.iter(SeqNo::MAX, None)
-            .rev()
-            .flat_map(|x| x.key())
-            .count(),
-        ITEM_COUNT,
-    );
-    assert!(src.lock_active_memtable().is_empty());
+    //     src.ingest(
+    //         (0..ITEM_COUNT as u64).map(|x| {
+    //             let k = x.to_be_bytes();
+    //             let v = nanoid::nanoid!();
+    //             (k.into(), v.into())
+    //         }),
+    //         &seqno,
+    //         &visible_seqno,
+    //     )?;
 
-    let folder = tempfile::tempdir()?;
-    let dest = Config::new(folder).open()?;
+    //     assert_eq!(src.len(SeqNo::MAX, None)?, ITEM_COUNT);
+    //     assert_eq!(
+    //         src.iter(SeqNo::MAX, None).flat_map(|x| x.key()).count(),
+    //         ITEM_COUNT,
+    //     );
+    //     assert_eq!(
+    //         src.iter(SeqNo::MAX, None)
+    //             .rev()
+    //             .flat_map(|x| x.key())
+    //             .count(),
+    //         ITEM_COUNT,
+    //     );
+    //     assert!(src.lock_active_memtable().is_empty());
 
-    dest.ingest(
-        src.iter(SeqNo::MAX, None)
-            .map(|x| x.into_inner())
-            .map(|x| x.unwrap()),
-        &seqno,
-        &visible_seqno,
-    )?;
+    //     let folder = tempfile::tempdir()?;
+    //     let dest = Config::new(folder).open()?;
 
-    assert_eq!(dest.len(SeqNo::MAX, None)?, ITEM_COUNT);
-    assert_eq!(
-        dest.iter(SeqNo::MAX, None).flat_map(|x| x.key()).count(),
-        ITEM_COUNT,
-    );
-    assert_eq!(
-        dest.iter(SeqNo::MAX, None)
-            .rev()
-            .flat_map(|x| x.key())
-            .count(),
-        ITEM_COUNT,
-    );
-    assert!(dest.lock_active_memtable().is_empty());
+    //     dest.ingest(
+    //         src.iter(SeqNo::MAX, None)
+    //             .map(|x| x.into_inner())
+    //             .map(|x| x.unwrap()),
+    //         &seqno,
+    //         &visible_seqno,
+    //     )?;
 
-    Ok(())
-}
+    //     assert_eq!(dest.len(SeqNo::MAX, None)?, ITEM_COUNT);
+    //     assert_eq!(
+    //         dest.iter(SeqNo::MAX, None).flat_map(|x| x.key()).count(),
+    //         ITEM_COUNT,
+    //     );
+    //     assert_eq!(
+    //         dest.iter(SeqNo::MAX, None)
+    //             .rev()
+    //             .flat_map(|x| x.key())
+    //             .count(),
+    //         ITEM_COUNT,
+    //     );
+    //     assert!(dest.lock_active_memtable().is_empty());
 
-#[test]
-#[ignore]
-fn blob_tree_bulk_ingest() -> lsm_tree::Result<()> {
-    let folder = tempfile::tempdir()?;
+    //     Ok(())
+    // }
 
-    let tree = Config::new(folder)
-        .blob_file_separation_threshold(1)
-        .open_as_blob_tree()?;
+    // #[test]
+    // #[ignore]
+    // fn blob_tree_bulk_ingest() -> lsm_tree::Result<()> {
+    //     let folder = tempfile::tempdir()?;
 
-    let seqno = SequenceNumberCounter::default();
-    let visible_seqno = SequenceNumberCounter::default();
+    //     let tree = Config::new(folder)
+    //         .blob_file_separation_threshold(1)
+    //         .open_as_blob_tree()?;
 
-    tree.ingest(
-        (0..ITEM_COUNT as u64).map(|x| {
-            let k = x.to_be_bytes();
-            let v = nanoid::nanoid!();
-            (k.into(), v.into())
-        }),
-        &seqno,
-        &visible_seqno,
-    )?;
+    //     let seqno = SequenceNumberCounter::default();
+    //     let visible_seqno = SequenceNumberCounter::default();
 
-    assert_eq!(tree.len(SeqNo::MAX, None)?, ITEM_COUNT);
-    assert_eq!(
-        tree.iter(SeqNo::MAX, None).flat_map(|x| x.key()).count(),
-        ITEM_COUNT,
-    );
-    assert_eq!(
-        tree.iter(SeqNo::MAX, None)
-            .rev()
-            .flat_map(|x| x.key())
-            .count(),
-        ITEM_COUNT,
-    );
-    assert_eq!(1, tree.blob_file_count());
+    //     tree.ingest(
+    //         (0..ITEM_COUNT as u64).map(|x| {
+    //             let k = x.to_be_bytes();
+    //             let v = nanoid::nanoid!();
+    //             (k.into(), v.into())
+    //         }),
+    //         &seqno,
+    //         &visible_seqno,
+    //     )?;
 
-    Ok(())
+    //     assert_eq!(tree.len(SeqNo::MAX, None)?, ITEM_COUNT);
+    //     assert_eq!(
+    //         tree.iter(SeqNo::MAX, None).flat_map(|x| x.key()).count(),
+    //         ITEM_COUNT,
+    //     );
+    //     assert_eq!(
+    //         tree.iter(SeqNo::MAX, None)
+    //             .rev()
+    //             .flat_map(|x| x.key())
+    //             .count(),
+    //         ITEM_COUNT,
+    //     );
+    //     assert_eq!(1, tree.blob_file_count());
+
+    //     Ok(())
 }
 
 #[test]
 #[ignore]
 fn blob_tree_copy() -> lsm_tree::Result<()> {
-    let folder = tempfile::tempdir()?;
-    let src = Config::new(folder)
-        .blob_file_separation_threshold(1)
-        .open_as_blob_tree()?;
+    todo!()
 
-    let seqno = SequenceNumberCounter::default();
-    let visible_seqno = SequenceNumberCounter::default();
+    // let folder = tempfile::tempdir()?;
+    // let src = Config::new(folder)
+    //     .blob_file_separation_threshold(1)
+    //     .open_as_blob_tree()?;
 
-    src.ingest(
-        (0..ITEM_COUNT as u64).map(|x| {
-            let k = x.to_be_bytes();
-            let v = nanoid::nanoid!();
-            (k.into(), v.into())
-        }),
-        &seqno,
-        &visible_seqno,
-    )?;
+    // let seqno = SequenceNumberCounter::default();
+    // let visible_seqno = SequenceNumberCounter::default();
 
-    assert_eq!(src.len(SeqNo::MAX, None)?, ITEM_COUNT);
-    assert_eq!(
-        src.iter(SeqNo::MAX, None).flat_map(|x| x.key()).count(),
-        ITEM_COUNT,
-    );
-    assert_eq!(
-        src.iter(SeqNo::MAX, None)
-            .rev()
-            .flat_map(|x| x.key())
-            .count(),
-        ITEM_COUNT,
-    );
-    assert!(src.lock_active_memtable().is_empty());
-    assert_eq!(1, src.blob_file_count());
+    // src.ingest(
+    //     (0..ITEM_COUNT as u64).map(|x| {
+    //         let k = x.to_be_bytes();
+    //         let v = nanoid::nanoid!();
+    //         (k.into(), v.into())
+    //     }),
+    //     &seqno,
+    //     &visible_seqno,
+    // )?;
 
-    let folder = tempfile::tempdir()?;
-    let dest = Config::new(folder)
-        .blob_file_separation_threshold(1)
-        .open_as_blob_tree()?;
+    // assert_eq!(src.len(SeqNo::MAX, None)?, ITEM_COUNT);
+    // assert_eq!(
+    //     src.iter(SeqNo::MAX, None).flat_map(|x| x.key()).count(),
+    //     ITEM_COUNT,
+    // );
+    // assert_eq!(
+    //     src.iter(SeqNo::MAX, None)
+    //         .rev()
+    //         .flat_map(|x| x.key())
+    //         .count(),
+    //     ITEM_COUNT,
+    // );
+    // assert!(src.lock_active_memtable().is_empty());
+    // assert_eq!(1, src.blob_file_count());
 
-    dest.ingest(
-        src.iter(SeqNo::MAX, None)
-            .map(|x| x.into_inner())
-            .map(|x| x.unwrap()),
-        &seqno,
-        &visible_seqno,
-    )?;
+    // let folder = tempfile::tempdir()?;
+    // let dest = Config::new(folder)
+    //     .blob_file_separation_threshold(1)
+    //     .open_as_blob_tree()?;
 
-    assert_eq!(dest.len(SeqNo::MAX, None)?, ITEM_COUNT);
-    assert_eq!(
-        dest.iter(SeqNo::MAX, None).flat_map(|x| x.key()).count(),
-        ITEM_COUNT,
-    );
-    assert_eq!(
-        dest.iter(SeqNo::MAX, None)
-            .rev()
-            .flat_map(|x| x.key())
-            .count(),
-        ITEM_COUNT,
-    );
-    assert!(dest.lock_active_memtable().is_empty());
-    assert_eq!(1, dest.blob_file_count());
+    // dest.ingest(
+    //     src.iter(SeqNo::MAX, None)
+    //         .map(|x| x.into_inner())
+    //         .map(|x| x.unwrap()),
+    //     &seqno,
+    //     &visible_seqno,
+    // )?;
 
-    Ok(())
+    // assert_eq!(dest.len(SeqNo::MAX, None)?, ITEM_COUNT);
+    // assert_eq!(
+    //     dest.iter(SeqNo::MAX, None).flat_map(|x| x.key()).count(),
+    //     ITEM_COUNT,
+    // );
+    // assert_eq!(
+    //     dest.iter(SeqNo::MAX, None)
+    //         .rev()
+    //         .flat_map(|x| x.key())
+    //         .count(),
+    //     ITEM_COUNT,
+    // );
+    // assert!(dest.lock_active_memtable().is_empty());
+    // assert_eq!(1, dest.blob_file_count());
+
+    // Ok(())
 }
