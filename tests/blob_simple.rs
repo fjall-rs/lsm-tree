241373a493e9ba3e7dafd5c8feede734fbcadf98
marvin-j97 <marvin.janke.97@gmail.com>
2025-09-24 19:25:42 +0200

    wip

--- tests/blob_simple.rs
+++ tests/blob_simple.rs
@@ -62,40 +62,42 @@ fn blob_tree_simple() -> lsm_tree::Result<()> {
 #[test]
 #[ignore = "wip"]
 fn blob_tree_simple_compressed() -> lsm_tree::Result<()> {
-    let folder = tempfile::tempdir()?;
-    let path = folder.path();
+    todo!()
 
-    let tree = lsm_tree::Config::new(path)
-        .compression(lsm_tree::CompressionType::Lz4)
-        .open_as_blob_tree()?;
+    // let folder = tempfile::tempdir()?;
+    // let path = folder.path();
 
-    let big_value = b"neptune!".repeat(128_000);
+    // let tree = lsm_tree::Config::new(path)
+    //     .compression(lsm_tree::CompressionType::Lz4)
+    //     .open_as_blob_tree()?;
 
-    assert!(tree.get("big", SeqNo::MAX)?.is_none());
-    tree.insert("big", &big_value, 0);
-    tree.insert("smol", "small value", 0);
+    // let big_value = b"neptune!".repeat(128_000);
 
-    let value = tree.get("big", SeqNo::MAX)?.expect("should exist");
-    assert_eq!(&*value, big_value);
+    // assert!(tree.get("big", SeqNo::MAX)?.is_none());
+    // tree.insert("big", &big_value, 0);
+    // tree.insert("smol", "small value", 0);
 
-    tree.flush_active_memtable(0)?;
+    // let value = tree.get("big", SeqNo::MAX)?.expect("should exist");
+    // assert_eq!(&*value, big_value);
 
-    let value = tree.get("big", SeqNo::MAX)?.expect("should exist");
-    assert_eq!(&*value, big_value);
+    // tree.flush_active_memtable(0)?;
 
-    let value = tree.get("smol", SeqNo::MAX)?.expect("should exist");
-    assert_eq!(&*value, b"small value");
+    // let value = tree.get("big", SeqNo::MAX)?.expect("should exist");
+    // assert_eq!(&*value, big_value);
 
-    let new_big_value = b"winter!".repeat(128_000);
-    tree.insert("big", &new_big_value, 1);
+    // let value = tree.get("smol", SeqNo::MAX)?.expect("should exist");
+    // assert_eq!(&*value, b"small value");
 
-    let value = tree.get("big", SeqNo::MAX)?.expect("should exist");
-    assert_eq!(&*value, new_big_value);
+    // let new_big_value = b"winter!".repeat(128_000);
+    // tree.insert("big", &new_big_value, 1);
 
-    tree.flush_active_memtable(0)?;
+    // let value = tree.get("big", SeqNo::MAX)?.expect("should exist");
+    // assert_eq!(&*value, new_big_value);
 
-    let value = tree.get("big", SeqNo::MAX)?.expect("should exist");
-    assert_eq!(&*value, new_big_value);
+    // tree.flush_active_memtable(0)?;
 
-    Ok(())
+    // let value = tree.get("big", SeqNo::MAX)?.expect("should exist");
+    // assert_eq!(&*value, new_big_value);
+
+    // Ok(())
 }
