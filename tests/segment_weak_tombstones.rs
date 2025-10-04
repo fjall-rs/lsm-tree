3d606aa2ae44db0392336d86b78a664f62dc4b25
Marvin <33938500+marvin-j97@users.noreply.github.com>
2025-10-04 22:17:39 +0200

    Update segment_weak_tombstones.rs

--- tests/segment_weak_tombstones.rs
+++ tests/segment_weak_tombstones.rs
@@ -36,3 +36,22 @@ fn weak_tombstone_counts_multiple_keys() -> lsm_tree::Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn weak_tombstone_counts_multiple_weak() -> lsm_tree::Result<()> {
+    let folder = tempfile::tempdir()?;
+    let tree = Config::new(folder.path()).open()?;
+
+    tree.insert(b"a", b"old", 10);
+    tree.remove_weak(b"a", 11);
+    tree.remove_weak(b"a", 12);
+    tree.remove_weak(b"a", 13);
+    tree.remove_weak(b"a", 14);
+
+    tree.flush_active_memtable(0)?;
+
+    assert_eq!(tree.weak_tombstone_count(), 4);
+    assert_eq!(tree.weak_tombstone_reclaimable_count(), 1); // a:10 is paired with a:11
+
+    Ok(())
+}
