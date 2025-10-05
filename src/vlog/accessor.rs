9f35f1dbb5b72ea73309a735412a2684bf6835a0
marvin-j97 <marvin.janke.97@gmail.com>
2025-10-05 21:33:58 +0200

    change vlog fd caching to need an Arc clone less

--- src/vlog/accessor.rs
+++ src/vlog/accessor.rs
@@ -43,18 +43,23 @@ impl<'a> Accessor<'a> {
 
         let bf_id = GlobalSegmentId::from((tree_id, blob_file.id()));
 
-        let file = if let Some(fd) = descriptor_table.access_for_blob_file(&bf_id) {
+        let cached_fd = descriptor_table.access_for_blob_file(&bf_id);
+        let fd_cache_miss = cached_fd.is_none();
+
+        let file = if let Some(fd) = cached_fd {
             fd
         } else {
-            let file = Arc::new(File::open(
+            Arc::new(File::open(
                 base_path.join(vhandle.blob_file_id.to_string()),
-            )?);
-            descriptor_table.insert_for_blob_file(bf_id, file.clone());
-            file
+            )?)
         };
 
         let value = Reader::new(blob_file, &file).get(key, vhandle)?;
         cache.insert_blob(tree_id, vhandle, value.clone());
+
+        if fd_cache_miss {
+            descriptor_table.insert_for_blob_file(bf_id, file);
+        }
 
         Ok(Some(value))
     }
