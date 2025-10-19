796cfb57dd4f0404261208585f48ffb08982308a
marvin-j97 <marvin.janke.97@gmail.com>
2025-10-19 23:10:53 +0200

    update sfa

--- src/compaction/state/mod.rs
+++ src/compaction/state/mod.rs
@@ -27,7 +27,7 @@ pub fn persist_version(folder: &Path, version: &Version) -> crate::Result<()> {
     let path = folder.join(format!("v{}", version.id()));
     let file = std::fs::File::create_new(path)?;
     let writer = BufWriter::new(file);
-    let mut writer = sfa::Writer::into_writer(writer);
+    let mut writer = sfa::Writer::from_writer(writer);
 
     version.encode_into(&mut writer)?;
 
