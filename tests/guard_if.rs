84ffa4bf4f196222119c512ff708ea00e4c9dd02
marvin-j97 <marvin.janke.97@gmail.com>
2025-11-09 23:57:00 +0100

    test: guard into inner if

---
+++ tests/guard_if.rs
@@ -0,0 +1,29 @@
+use lsm_tree::{AbstractTree, Config, Guard, SeqNo, SequenceNumberCounter};
+use test_log::test;
+
+#[test]
+fn guard_into_inner_if() -> lsm_tree::Result<()> {
+    let folder = tempfile::tempdir()?;
+
+    {
+        let tree = Config::new(&folder, SequenceNumberCounter::default()).open()?;
+
+        tree.insert("a#name", "peter", 0);
+        tree.insert("a#age", "25", 0);
+
+        assert_eq!(2, tree.iter(SeqNo::MAX, None).count());
+
+        assert_eq!(
+            1,
+            tree.iter(SeqNo::MAX, None)
+                .filter_map(|guard| {
+                    guard
+                        .into_inner_if(|key| key.ends_with(b"#name"))
+                        .transpose()
+                })
+                .count(),
+        );
+    }
+
+    Ok(())
+}
