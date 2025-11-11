48ab2666cb1cc7f19a29a4dd96c9e838bb1dadb5
marvin-j97 <marvin.janke.97@gmail.com>
2025-11-11 22:45:23 +0100

    improve test case

--- tests/tree_recover_counter.rs
+++ tests/tree_recover_counter.rs
@@ -5,7 +5,7 @@ use test_log::test;
 fn tree_recover_table_counter() -> lsm_tree::Result<()> {
     let folder = tempfile::tempdir()?;
 
-    {
+    let counter_expected = {
         let tree = Config::new(&folder, SequenceNumberCounter::default()).open()?;
 
         assert_eq!(0, tree.next_table_id());
@@ -13,18 +13,17 @@ fn tree_recover_table_counter() -> lsm_tree::Result<()> {
         tree.insert("a", "a", 0);
         tree.flush_active_memtable(0)?;
 
-        assert_eq!(1, tree.next_table_id());
+        assert_eq!(2, tree.next_table_id());
 
         tree.insert("b", "b", 0);
         tree.flush_active_memtable(0)?;
 
-        assert_eq!(2, tree.next_table_id());
-    }
+        tree.next_table_id()
+    };
 
     {
         let tree = Config::new(&folder, SequenceNumberCounter::default()).open()?;
-
-        assert_eq!(2, tree.next_table_id());
+        assert_eq!(counter_expected, tree.next_table_id());
     }
 
     Ok(())
