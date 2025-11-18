f7c8913bf0b31c790d7b8742686ce12a66a485e6
marvin-j97 <marvin.janke.97@gmail.com>
2025-11-18 20:03:05 +0100

    update sfa

--- src/checksum.rs
+++ src/checksum.rs
@@ -42,3 +42,42 @@ impl Checksum {
         }
     }
 }
+
+pub struct ChecksummedWriter<W: std::io::Write> {
+    inner: W,
+    hasher: xxhash_rust::xxh3::Xxh3Default,
+}
+
+impl<W: std::io::Write + std::io::Seek> std::io::Seek for ChecksummedWriter<W> {
+    fn seek(&mut self, pos: std::io::SeekFrom) -> std::io::Result<u64> {
+        self.inner.seek(pos)
+    }
+}
+
+impl<W: std::io::Write> ChecksummedWriter<W> {
+    pub fn new(writer: W) -> Self {
+        Self {
+            inner: writer,
+            hasher: xxhash_rust::xxh3::Xxh3Default::new(),
+        }
+    }
+
+    pub fn checksum(&self) -> Checksum {
+        Checksum::from_raw(self.hasher.digest128())
+    }
+
+    pub fn inner_mut(&mut self) -> &mut W {
+        &mut self.inner
+    }
+}
+
+impl<W: std::io::Write> std::io::Write for ChecksummedWriter<W> {
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.inner.flush()
+    }
+
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.hasher.update(buf);
+        self.inner.write(buf)
+    }
+}
