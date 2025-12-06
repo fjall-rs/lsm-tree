76f3c8bcc912dfbaa3a98472026878af5563a952
zaidoon <zaidoon@cloudflare.com>
2025-12-06 17:25:13 -0500

    Add checksum verification API

--- src/lib.rs
+++ src/lib.rs
@@ -145,6 +145,7 @@ pub mod util;
 
 mod value;
 mod value_type;
+mod verify;
 mod version;
 mod vlog;
 
@@ -189,6 +190,10 @@ pub use {
     tree::Tree,
     value::SeqNo,
     value_type::ValueType,
+    verify::{
+        CancellationToken, CorruptedFile, FileType, VerificationIoError, VerificationOptions,
+        VerificationProgress, VerificationResult,
+    },
     vlog::BlobFile,
 };
 
