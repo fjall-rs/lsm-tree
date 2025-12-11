4b03341f73a5c2c74223abd3afe4f33546b8ad69
marvin-j97 <marvin.janke.97@gmail.com>
2025-12-11 13:28:07 +0100

    test and refactor

--- src/key.rs
+++ src/key.rs
@@ -12,22 +12,9 @@ pub struct InternalKey {
     pub value_type: ValueType,
 }
 
-impl AsRef<[u8]> for InternalKey {
-    fn as_ref(&self) -> &[u8] {
-        &self.user_key
-    }
-}
-
 impl PartialEq for InternalKey {
     fn eq(&self, other: &Self) -> bool {
         self.user_key == other.user_key && self.seqno == other.seqno
-    }
-}
-
-impl std::hash::Hash for InternalKey {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        state.write(&self.user_key);
-        state.write_u64(self.seqno);
     }
 }
 
