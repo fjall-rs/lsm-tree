d62f2cc0067fd67dcaaf6f308a562e9bca1a1a73
asonix <asonix@asonix.dog>
2026-01-09 23:08:23 -0600

    Add tests for into_inner_if_some

--- tests/guard_if.rs
+++ tests/guard_if.rs
@@ -69,3 +69,68 @@ fn guard_into_inner_if_blob() -> lsm_tree::Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn guard_into_inner_if_some() -> lsm_tree::Result<()> {
+    let folder = get_tmp_folder();
+
+    {
+        let tree = Config::new(
+            &folder,
+            SequenceNumberCounter::default(),
+            SequenceNumberCounter::default(),
+        )
+        .open()?;
+
+        tree.insert("earth#name", "earth", 0);
+        tree.insert("earth#color", "BLUE", 0);
+
+        assert_eq!(2, tree.iter(SeqNo::MAX, None).count());
+
+        assert_eq!(
+            1,
+            tree.iter(SeqNo::MAX, None)
+                .filter_map(|guard| {
+                    guard
+                        .into_inner_if_some(|key| if key.ends_with(b"#name") { Some(()) } else { None })
+                        .unwrap()
+                })
+                .count(),
+        );
+    }
+
+    Ok(())
+}
+
+#[test]
+fn guard_into_inner_if_some_blob() -> lsm_tree::Result<()> {
+    let folder = get_tmp_folder();
+
+    {
+        let tree = Config::new(
+            &folder,
+            SequenceNumberCounter::default(),
+            SequenceNumberCounter::default(),
+        )
+        .with_kv_separation(Some(KvSeparationOptions::default().separation_threshold(1)))
+        .open()?;
+
+        tree.insert("earth#name", "earth", 0);
+        tree.insert("earth#color", "BLUE", 0);
+
+        assert_eq!(2, tree.iter(SeqNo::MAX, None).count());
+
+        assert_eq!(
+            1,
+            tree.iter(SeqNo::MAX, None)
+                .filter_map(|guard| {
+                    guard
+                        .into_inner_if_some(|key| if key.ends_with(b"#name") { Some(()) } else { None })
+                        .unwrap()
+                })
+                .count(),
+        );
+    }
+
+    Ok(())
+}
