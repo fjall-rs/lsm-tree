94687c6915d2a4c57ddf163d5ea8e85e4b858d1f
marvin-j97 <marvin.janke.97@gmail.com>
2026-02-10 21:16:24 +0100

    refactor

--- tests/compaction_filter_snapshot.rs
+++ tests/compaction_filter_snapshot.rs
@@ -3,30 +3,30 @@ use lsm_tree::compaction::filter::{
 };
 use lsm_tree::{get_tmp_folder, AbstractTree, SeqNo, SequenceNumberCounter};
 
+struct NukeFilter;
+
+impl CompactionFilter for NukeFilter {
+    fn filter_item(&mut self, _: ItemAccessor<'_>) -> lsm_tree::Result<FilterVerdict> {
+        // data? what data?
+        Ok(FilterVerdict::Remove)
+    }
+}
+
+struct NukeFilterFactory;
+
+impl CompactionFilterFactory for NukeFilterFactory {
+    fn make_filter(&self) -> Box<dyn CompactionFilter> {
+        Box::new(NukeFilter)
+    }
+}
+
 #[test]
 fn filter_snapshot() -> lsm_tree::Result<()> {
-    struct DropEverything;
-
-    impl CompactionFilter for DropEverything {
-        fn filter_item(&mut self, _: ItemAccessor<'_>) -> lsm_tree::Result<FilterVerdict> {
-            // data? what data?
-            Ok(FilterVerdict::Remove)
-        }
-    }
-
-    struct DropEverythingFactory;
-
-    impl CompactionFilterFactory for DropEverythingFactory {
-        fn make_filter(&self) -> Box<dyn CompactionFilter> {
-            Box::new(DropEverything)
-        }
-    }
-
     let folder = get_tmp_folder();
 
     let seqno = SequenceNumberCounter::default();
     let config = lsm_tree::Config::new(&folder, seqno.clone(), SequenceNumberCounter::default())
-        .with_compaction_filter_factory(Some(Box::new(DropEverythingFactory)));
+        .with_compaction_filter_factory(Some(Box::new(NukeFilterFactory)));
     let tree = config.open()?;
 
     tree.insert("a", "a", seqno.next());
